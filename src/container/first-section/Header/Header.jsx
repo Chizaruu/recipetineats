8e7cab4a443a727bf80f42496d8e2a72c4b2792d
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2022-05-05 12:28:08 +1000

    Created SocialButton, Added SocialButton to Header.jsx, Converted social buttons into SocialButtons, Added recipetineat.PNG
    Signed-off-by: Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>

--- src/container/first-section/Header/Header.jsx
+++ src/container/first-section/Header/Header.jsx
@@ -1,46 +1,55 @@
+import SocialButton from "./../../../components/first-section/SocialButton";
 import styles from "./Header.module.scss";
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <div>
-                <h1 className={styles.header__title_section}>
+            <div className={styles.header__wrapper}>
+                <div className={styles.header__wrapper__title_section}>
                     <a
-                        className={styles.header__title_section__title}
+                        className={styles.header__wrapper__title_section__title}
                         href="https://github.com/Chizaruu/recipetineats"
                         rel="noopener noreferrer"
                         target="_blank"
                     >
-                        Recipetineats
+                        recipe<span>tin</span>eats
                     </a>
-                </h1>
-            </div>
-            <div className={styles.header__search_section}>
-                <input
-                    className={styles.header__search_section__input}
-                    type="text"
-                    placeholder="Search Recipes..."
-                />
-                <button
-                    className={styles.header__search_section__search}
-                    type="submit"
-                >
-                    <img src="./images/search-icon.png" alt="?" />
-                </button>
-            </div>
-            <div className={styles.header__social_section}>
-                <a className={styles.header__social_section__social} href="#">
-                    <img src="./images/instagram-icon.png" alt="instagram" />
-                </a>
-                <a className={styles.header__social_section__social} href="#">
-                    <img src="./images/facebook-icon.png" alt="facebook" />
-                </a>
-                <a className={styles.header__social_section__social} href="#">
-                    <img src="./images/pinterest-icon.png" alt="pinterest" />
-                </a>
-                <a className={styles.header__social_section__social} href="#">
-                    <img src="./images/youtube-icon.png" alt="youtube" />
-                </a>
+                </div>
+                <div className={styles.header__wrapper__search_section}>
+                    <input
+                        className={
+                            styles.header__wrapper__search_section__input
+                        }
+                        type="text"
+                        placeholder="Search Recipes..."
+                    />
+                    <button
+                        className={
+                            styles.header__wrapper__search_section__search
+                        }
+                        type="submit"
+                    >
+                        <img src="./images/search-icon.png" alt="?" />
+                    </button>
+                </div>
+                <div className={styles.header__wrapper__social_section}>
+                    <SocialButton
+                        srcText="./images/instagram-icon.png"
+                        altText="Instagram"
+                    />
+                    <SocialButton
+                        srcText="./images/facebook-icon.png"
+                        altText="Facebook"
+                    />
+                    <SocialButton
+                        srcText="./images/pinterest-icon.png"
+                        altText="Pinterest"
+                    />
+                    <SocialButton
+                        srcText="./images/youtube-icon.png"
+                        altText="Youtube"
+                    />
+                </div>
             </div>
         </header>
     );
